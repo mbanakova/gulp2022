c729b5bf38dbfbca0f3d3e6f55be91c6cf02dbce
mbanakova <mbanakova@yandex.ru>
2022-11-09 22:55:43 +0300

    img to dist

--- gulp/config/path.js
+++ gulp/config/path.js
@@ -6,16 +6,23 @@ const srcFolder = `./src`
 
 export const path = {
   build: {
+    js: `${buildFolder}/js/`,
+    images: `${buildFolder}/img/`,
     css: `${buildFolder}/css/`,
     html: `${buildFolder}/`,
     files: `${buildFolder}/files/`
   },
   src: {
+    js: `${srcFolder}/js/app.js`,
+    images: `${srcFolder}/img/**/*.{jpg,jpeg,png,gif,webp}`,
+    svg: `${srcFolder}/img/**/*.svg`,
     scss: `${srcFolder}/scss/style.scss`,
     html: `${srcFolder}/*.html`,
     files: `${srcFolder}/files/**/*.*`
   },
   watch: {
+    js: `${srcFolder}/js/**/*.js`,
+    images: `${srcFolder}/img/**/*.{jpg,jpeg,png,svg,ico,gif,webp}`,
     scss: `${srcFolder}/scss/**/*.scss`,
     html: `${srcFolder}/**/*.html`,
     files: `${srcFolder}/files/**/*.*`
